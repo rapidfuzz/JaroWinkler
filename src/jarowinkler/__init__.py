a972f129e5c081d59ce06901a8497bcbda91ad44
Max Bachmann <max.bachmann@iracing.com>
2022-07-04 15:29:29 +0200

    release v1.1.0

--- src/jarowinkler/__init__.py
+++ src/jarowinkler/__init__.py
@@ -1,6 +1,6 @@
 __author__: str = "Max Bachmann"
 __license__: str = "MIT"
-__version__: str = "1.0.5"
+__version__: str = "1.1.0"
 
 def _fallback_import(module: str, name: str):
     import importlib
