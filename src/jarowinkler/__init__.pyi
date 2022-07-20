6cc162a1bee9b67718c5387a09bb5214618099da
Max Bachmann <kontakt@maxbachmann.de>
2022-07-20 22:35:48 +0200

    fix type hints

--- src/jarowinkler/__init__.pyi
+++ src/jarowinkler/__init__.pyi
@@ -5,16 +5,16 @@ __license__: str
 __version__: str
 
 _StringType = Sequence[Hashable]
-S1 = TypeVar("S1")
-S2 = TypeVar("S2")
+_S1 = TypeVar("S1")
+_S2 = TypeVar("S2")
 
 def jaro_similarity(
-    s1: S1, s2: S2, *,
-    processor: Optional[Callable[[Union[S1, S2]], _StringType]] = None,
+    s1: _S1, s2: _S2, *,
+    processor: Optional[Callable[[Union[_S1, _S2]], _StringType]] = None,
     score_cutoff: Optional[float] = 0) -> float: ...
 
 def jarowinkler_similarity(
-    s1: S1, s2: S2, *,
+    s1: _S1, s2: _S2, *,
     prefix_weight: float = 0.1,
-    processor: Optional[Callable[[Union[S1, S2]], _StringType]] = None,
+    processor: Optional[Callable[[Union[_S1, _S2]], _StringType]] = None,
     score_cutoff: Optional[float] = 0) -> float: ...
