35d3b9e08999f75dfbf07c97a68955609eeb2f91
Max Bachmann <oss@maxbachmann.de>
2023-11-01 22:23:19 +0100

    fix tests

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -2,16 +2,12 @@ from string import ascii_letters, digits, punctuation
 
 from hypothesis import given, settings
 import hypothesis.strategies as st
-from jarowinkler import _initialize_py, _initialize_cpp
+from jarowinkler import jarowinkler_similarity
+
 
 def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
     return abs(a - b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
 
-def jarowinkler_similarity(*args, **kwargs):
-    sim1 = _initialize_py.jarowinkler_similarity(*args, **kwargs)
-    sim2 = _initialize_cpp.jarowinkler_similarity(*args, **kwargs)
-    assert isclose(sim1, sim2)
-    return sim1
 
 def jaro_similarity(P, T):
     P_flag = [0] * (len(P) + 1)
@@ -51,8 +47,13 @@ def jaro_similarity(P, T):
 
     Transpositions = Transpositions // 2
 
-    Sim = CommonChars / len(P) + CommonChars / len(T) + (CommonChars - Transpositions) / CommonChars
+    Sim = (
+        CommonChars / len(P)
+        + CommonChars / len(T)
+        + (CommonChars - Transpositions) / CommonChars
+    )
     return Sim / 3
+
 
 def jaro_winkler_similarity(P, T, prefix_weight=0.1):
     min_len = min(len(P), len(T))
@@ -71,21 +72,20 @@ def jaro_winkler_similarity(P, T, prefix_weight=0.1):
     return Sim
 
 
-def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
-    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
-
-
 HYPOTHESIS_ALPHABET = ascii_letters + digits + punctuation
+
 
 @given(s1=st.text(max_size=64), s2=st.text(max_size=64))
 @settings(max_examples=50, deadline=1000)
 def test_jaro_winkler_word(s1, s2):
     assert isclose(jaro_winkler_similarity(s1, s2), jarowinkler_similarity(s1, s2))
 
+
 @given(s1=st.text(min_size=65), s2=st.text(min_size=65))
 @settings(max_examples=50, deadline=1000)
 def test_jaro_winkler_block(s1, s2):
     assert isclose(jaro_winkler_similarity(s1, s2), jarowinkler_similarity(s1, s2))
+
 
 @given(s1=st.text(), s2=st.text())
 @settings(max_examples=50, deadline=1000)
